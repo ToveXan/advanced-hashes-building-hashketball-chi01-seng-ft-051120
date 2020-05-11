9dc9200b4261d1a62d1a0123e34a28f50b73490b
Zoe Xanos <zxanos@gmail.com>
2020-05-11 09:04:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -39,7 +39,7 @@ def game_hash
           blocks: 1,
           slam_dunks: 15
         },
-      {
+        {
           player_name: "Mason Plumlee",
           number: 1,
           shoe: 19,
@@ -48,7 +48,8 @@ def game_hash
           assists: 6,
           steals: 3,
           blocks: 8,
-          
+          slam_dunks: 5
+        },
       },
         player_name: "JasonTerry" => {
         number: 0,
