bbc5f688170ff3a271fe732e0da62067643f406e
Zoe Xanos <zxanos@gmail.com>
2020-05-11 08:27:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,8 +3,8 @@ require 'pry'
 def game_hash 
   { 
   home: {
-    team_name: {"Brooklyn Nets"}, 
-    colors: {["Black", "White"]}, 
+    team_name: "Brooklyn Nets", 
+    colors: ["Black", "White"], 
     players: {
       "Alan Anderson" => {
         number: 0,
@@ -58,8 +58,8 @@ def game_hash
       },
     }, 
   away: {
-    team_name: {"Charlotte Hornets"}, 
-    colors: {["Turquoise", "Purple"]}, 
+    team_name: "Charlotte Hornets", 
+    colors: ["Turquoise", "Purple"], 
     players: {
       "Jeff Adrien" => {
         number: 4,
